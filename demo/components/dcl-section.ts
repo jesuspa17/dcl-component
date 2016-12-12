8f1111ee9b4bfc414cfc1cb18996c5fbedae384f
Jose Antonio Delgado Alfonso <jose.delgado@aoifes.com>
2016-12-12 16:43:29 +0100

    Add onChange

--- demo/components/dcl-section.ts
+++ demo/components/dcl-section.ts
@@ -8,8 +8,9 @@ import {DemoComponent} from './demo-component';
   <br>
   <section>
     <hr>
+    <button (click)="pruebaClick()">Prueba</button>
     <div>
-      {{_data}}
+      {{_dataOut}}
       <dclcomponent [type]="_component"
                      [init]="_func"
                      [data]="_data">
@@ -27,6 +28,7 @@ export class DCLSection {
   private _func: any;
 
   private _data2: number = 0;
+  private _dataOut: number = 1;
 
   private value: number = 0;
 
@@ -41,8 +43,23 @@ export class DCLSection {
     console.log(component.instance.cOutput);
     component.instance.cOutput.subscribe((val: number) => {
       console.log('emitted: %d', val);
+      this._dataOut = val;
+    });
+  }
+
+  private pruebaInit2(component: ComponentRef<any>, data: any): void {
+    console.log(this.value);
+    console.log('Private variable value: %d and data: %d', this.value, this._data);
+    component.instance._campo = this._data;
+    console.log(component.instance.cOutput);
+    component.instance.cOutput.subscribe((val: number) => {
+      console.log('emitted: %d', val);
       this._data = val;
     });
+  }
+
+  private pruebaClick() {
+    this._func = this.pruebaInit2.bind(this);
   }
 
 }
